94da171c3c8781c44ac631d78437ee47517052b6
hitalo20 <hitalo.chevonica@escola.pr.gov.br>
2025-03-14 19:43:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let audios = [
     {caminho:'Iframe Bruto de Raça - Antony & Gabriel, João Nelore & Texano part. Marco Brasil (Clipe Oficial) [pbE0Zxenmao] (2).mp3', legenda:'Matuto de verdade'},
-    {caminho:'Arash.mp3', legenda:'Arash'},
+    {caminho:'Iframe Caminhoneta Alta - Dan Lellis, Caju, Cintia Souza & Junim Dantes ft. Rincon 061 (Oficial Videoclipe) [qdJd5YrCdrw].mp3', legenda:'Arash'},
     {caminho:'Paula Fernandes.mp3', legenda:'Paula Fernandes'},
     {caminho:'Hayit.mp3', legenda:'Hayit'},
     {caminho:'Helena.mp3', legenda:'Helena'},
